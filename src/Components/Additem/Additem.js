6cc6ab6bd609d3fe77a65ebc6f1523803bd6fc37
Hazem-Farhati <79176361+Hazem-Farhati@users.noreply.github.com>
2022-07-29 09:50:14 +0100

    last one

--- src/Components/Additem/Additem.js
+++ src/Components/Additem/Additem.js
@@ -9,16 +9,16 @@ const Additem = ({movies,setMovies}) => {
     poster:""
 
   });
-  const handleSave = ()=>{setMovies([...movies,newmovie])}
+  const handleSave = ()=>{setMovies([newmovie,...movies])}
   return (
     < >
-  <button  className='add-btn' onClick={()=>setShow(!show)}>
+  <button  className='add-btn' onClick={()=>setShow(true)}>
        Add ur movie
       </button>
       {  show? 
       
       <div className='all-inpt'>
-        <input  className='inpt' type="text" name="title" placeholder='add ur title' onChange={ (e) =>  setNewmovie({ ...newmovie, title: e.target.value, })}/>        
+        <input  className='inpt'  type="text" name="title" placeholder='add ur title' onChange={ (e) =>  setNewmovie({ ...newmovie, title: e.target.value, })}/>        
         <br /><input className='inpt' type="text" name="description" placeholder='add ur Description' onChange={ e => setNewmovie({ ...newmovie, description: e.target.value, })}/>
         <br /><input className='inpt' type="text" name="poster" placeholder='add ur Url' onChange={ e=>   setNewmovie({ ...newmovie, poster: e.target.value, })
 }/>
@@ -26,11 +26,16 @@ const Additem = ({movies,setMovies}) => {
       </div>  :null
       }
       { show?
-      <button className='save-btn'  onClick={handleSave}>
+      <button className='save-btn'  onClick={handleSave} >
             Save
           </button> 
-          :null}
           
+          :null}
+           { show?
+             <button  className='close-btn button4' onClick={()=>setShow(false)}>
+       X
+      </button>
+          :null};
     </>
   );
 }
